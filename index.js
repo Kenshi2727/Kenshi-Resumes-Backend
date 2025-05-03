8853fa4bd35ce5fcdaa0c2cc5d9444e884e3d62c
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-05-03 14:27:26 +0530

    configured ssl

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ const db = new pg.Client({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 db.connect();
 
